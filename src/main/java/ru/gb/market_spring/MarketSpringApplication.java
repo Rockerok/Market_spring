e9c6ded47b1d535b197a17c72b4bea12387193d7
Rockerok <rockerok@mail.ru>
2022-12-13 22:17:02 +0300

    Initial commit

---
+++ src/main/java/ru/gb/market_spring/MarketSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.market_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/market_spring/MarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.market_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

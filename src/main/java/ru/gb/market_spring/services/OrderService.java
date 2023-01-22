624c7971b03729596566bfedfc6b1a92ee68934b
Rockerok <rockerok@mail.ru>
2023-01-22 15:15:18 +0300

    Home Work5 подготовка

--- src/main/java/ru/gb/market_spring/services/OrderService.java
+++ src/main/java/ru/gb/market_spring/services/OrderService.java
@@ -2,52 +2,52 @@ package ru.gb.market_spring.services;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.gb.market_spring.entities.*;
+import ru.gb.market_spring.exceptions.ResourceNotFoundException;
 import ru.gb.market_spring.repositories.OrderItemRepository;
 import ru.gb.market_spring.repositories.OrderRepository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OrderService {
-    private ProductService productService;
-    private CartService cartService;
-    private OrderRepository ordersRepository;
-    private OrderItemRepository orderItemRepository;
+    private final  CartService cartService;
+    private final  ProductService productService;
+
+    private final  OrderRepository ordersRepository;
+    private final  OrderItemRepository orderItemRepository;
 
 
+    @Transactional
     public void createOrder (User user){
         //TODO DZ-3 1:59:00
         Cart cart = cartService.getCurrentCart();
         Order order = new Order();
-        int totalPrice=0;
         order.setUser(user);
-//        order.setItems(orderItemRepository.findById(ordersRepository.findBy(order)));
-        for (OrderItem items: order.getItems()
-             ) {
-            totalPrice+= items.getPrice();
-        }
-        order.setTotal_price(totalPrice);
-        createOrderItems(cart,order);
+        order.setTotal_price(cart.getTotalPrice());
+//        order.setPhone();
+//        order.setAddress();
         ordersRepository.save(order);
-    }
 
-    private void createOrderItems(Cart cart, Order order) {
-        OrderItem orderItem = (OrderItem) cart.getItems().stream().map(cartItem -> new OrderItem(
-                null,
-                productService.findById(cartItem.getProductId()).orElseThrow(() -> new RuntimeException()), //todo RuntimeEx
-                order,
-                cartItem.getQuantity(),
-                cartItem.getPricePerProduct(),
-                cartItem.getPrice(),
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        ));
-
-
-        orderItemRepository.save(orderItem);
+        List<OrderItem> orderItems = cart.getItems().stream().map(
+                cartItem ->{
+                    OrderItem orderItem =  new OrderItem();
+                    orderItem.setProduct(productService.findById(cartItem.getProductId()).orElseThrow(
+                            () -> new ResourceNotFoundException("Нельзя добавить продукт id:"
+                                    +cartItem.getProductId()+ ". Такого продукта нет"))
+                    );
+                    orderItem.setOrder(order);
+                    orderItem.setQuantity(cartItem.getQuantity());
+                    orderItem.setPricePerProduct(cartItem.getPricePerProduct());
+                    orderItem.setPrice(cartItem.getPrice());
+                    orderItemRepository.save(orderItem);
+                    return orderItem;
+                }).collect(Collectors.toList());
     }
 }
